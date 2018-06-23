11c97c3d1b57a01f27f7f39c09e2a25ccc46eb09
root <root@voip>
2018-06-23 14:06:55 -0300

    Solucionado el select de cambiar usuario y se cambio DENUNCIA por REGISTRO

--- modules/denuncias/client/config/denuncias.client.config.js
+++ modules/denuncias/client/config/denuncias.client.config.js
@@ -10,7 +10,7 @@
   function menuConfig(menuService) {
     // Set top bar menu items
     menuService.addMenuItem('topbar', {
-      title: 'Denuncias',
+      title: 'Registros',
       state: 'denuncias',
       type: 'dropdown',
       roles: ['user', 'admin']
@@ -18,14 +18,14 @@
 
     // Add the dropdown list item
     menuService.addSubMenuItem('topbar', 'denuncias', {
-      title: 'Lista de Denuncias',
+      title: 'Listar Registros',
       state: 'denuncias.list',
       roles: ['user', 'admin']
     });
 
     // Add the dropdown create item
     menuService.addSubMenuItem('topbar', 'denuncias', {
-      title: 'Cargar Denuncia',
+      title: 'Cargar Registro',
       state: 'denuncias.create',
       roles: ['user', 'admin']
     });
